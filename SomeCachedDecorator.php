9f8ac999900a77b1f72849520a9218578c3ba3f4
diablo10076 <43439082+diablo10076@users.noreply.github.com>
2019-11-25 20:56:39 +0300

    Add files via upload

--- SomeCachedDecorator.php
+++ SomeCachedDecorator.php
@@ -3,15 +3,19 @@
 declare(strict_types=1);
 
 namespace src\Decorator;
-
+use Exception;
 use DateTime;
-use src\Cache\CacheItem;
+use Psr\Cache\CacheItemInterface;
 use Psr\Cache\CacheItemPoolInterface;
 use Psr\Log\LoggerInterface;
 use src\Integration\DataProviderInterface;
 
-class SomeCachedDecorator
+class SomeCachedDecorator implements DataProviderInterface
 {
+    /**
+     * @var CacheItemInterface
+     */
+    private $caching;
     /**
      * @var CacheItemPoolInterface
      */
@@ -43,40 +47,57 @@ class SomeCachedDecorator
      * @param array $input
      * @return array
      * @see DataProviderInterface::get()
+     * @throws \Psr\Cache\InvalidArgumentException
      */
     public function get(array $input): array
     {
-        $cacheItem = new CacheItem($input);
         try {
-            $cacheItem = $this->cache->getItem($cacheItem->getKey());
-            if ($cacheItem->isHit()) {
-                return $cacheItem->get();
+            if (!($this->caching instanceof CacheItemInterface)){
+                throw new Exception('wrong setter');
             }
-        } catch (\Psr\Cache\InvalidArgumentException $e) {
-            $this->logger->critical(
-                'cache getItem error',
+            try {
+                $cacheItem = $this->cache->getItem($this->caching->getKey());
+                if ($cacheItem->isHit()) {
+                    return $cacheItem->get();
+                }
+                $result = $this->provider->get($input);
+                $cacheItem
+                    ->set($result)
+                    ->expiresAt(
+                        (new DateTime())->modify('+1 day')
+                    );
+                if (!$this->cache->save($cacheItem)) {
+                    $this->logger->critical(
+                        'cache save error',
+                        [
+                            'cachePoolClassname' => get_class($this->cache),
+                            //  и любая другая информация чтобы было понимание почему данные не записались в кеш
+                        ]
+                    );
+                }
+            } catch (Exception $e) {
+                $this->logger->error(
+                    $e->getMessage(),
+                    [
+                        'exception' => $e
+                    ]
+                );
+            }
+        }catch (Exception $e){
+            $this->logger->error(
+                $e->getMessage(),
                 [
-                    'message' => $e->getMessage(),
-                    // + все доп. данные по исключению, ключ кеша, имя класса пула
+                    'exception' => $e
                 ]
             );
-        }
-        $result = $this->provider->get($input);
-        $cacheItem
-            ->set($result)
-            ->expiresAt(
-                (new DateTime())->modify('+1 day')
-            );
-        if (!$this->cache->save($cacheItem)) {
-            $this->logger->critical(
-                'cache save error',
-                [
-                    'cachePoolClassname' => get_class($this->cache),
-                    //  и любая другая информация чтобы было понимание почему данные не записались в кеш
-                ]
-            );
+
         }
 
-        return $result;
+        return [];
+    }
+
+    public function setCaching(CacheItemInterface $caching)
+    {
+        $this->caching = $caching;
     }
 }
